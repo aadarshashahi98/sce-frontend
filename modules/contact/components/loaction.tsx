7203b84c21ad1649a6335fe43419cf8dba453add
Aadarsha Shahi <aadarsha.shahi.1000@gmail.com>
2026-07-04 20:54:12 +0545

    Update in google map iframe

--- modules/contact/components/loaction.tsx
+++ modules/contact/components/loaction.tsx
@@ -2,7 +2,7 @@ export function Location() {
   return (
     <section>
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d230886.0381073419!2d51.34717952738604!3d25.284228025325085!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e45c534ffdce87f%3A0x44d9319f78cfd4b1!2sDoha%2C%20Qatar!5e0!3m2!1sen!2snp!4v1782636274140!5m2!1sen!2snp"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d902.0189957879946!2d51.519485929430395!3d25.268029282818127!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e45c52161ae27c3%3A0x2346a92fd8100c9d!2sSatipipal%20Contracting%20%26%20Electrical%20WLL!5e0!3m2!1sen!2sqa!4v1783177590666!5m2!1sen!2sqa"
         style={{ width: "100%"}}
         className="h-64 md:h-96 lg:h-[600px]"
         allowFullScreen
